bf6836a90d7c7024cf9841e49c39f05ae48a7944
Andrea <andreabarr87@gmail.com>
2019-12-20 11:22:54 -0600

    last

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,23 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+  end
+
+  def new
+    Heroine.new(heroine_params)
+  end
+
+private
+
+def heroine_params
+  params.require(:heroine).permit(:name, :username)
+end
+
 end
